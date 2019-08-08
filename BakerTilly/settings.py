5e339f75e80a7b23518dcc9df574c0abfc9300a3
sdtaylor_admin <sdtaylor_admin@staylor.nscg.com>
2019-08-08 09:30:48 -0300

    adjust database settings

--- BakerTilly/settings.py
+++ BakerTilly/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'BakerTilly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'DRIVER': os.environ.get('DB_DRIVER'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': '',
+
+        'OPTIONS': {
+            'driver': os.environ.get('DB_DRIVER'),
+        },
     },
 }
 
